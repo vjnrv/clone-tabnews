9015f6755aa2b8b18bcecd010155dbf4b69a9bcb
Pedro Junior <v.ju.ni.or.v@gmail.com>
2023-06-13 15:44:03 +0000

    day 4: home page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home () {
+    const style = {
+        margin: 0,
+        padding: '4 0vh 10px',
+        backgroundColor: 'red',
+        textTransform: 'uppercase',
+        color: '#ffffff',
+        textAlign: 'center',
+        lineHeight: '100px',
+        borderRadius: '20px'
+    }
+
+    return <div style={style}>
+        <h1>Oi Juciara. <br /> Te amo!</h1>
+    </div>
+}
+
+export default Home
